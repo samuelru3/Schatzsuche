17bb896e03e7bac9adcca8c05360e6130a295c3b
samuelru3 <72190083+samuelru3@users.noreply.github.com>
2021-01-18 00:23:14 +0100

    überprüfung

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
-const PokemonApp = {
+const SchatzApp = {
     data() {
         return {
             // --- Variablen zum Sichtbarmachen ---
             displayCode: true
 
         }
+    },
+
+    methods: {
+        ueberpruefen1() {
+            if (this.code1 == 1) {
+                console.log("1");
+            } else {
+                console.log("nicht");
+            }
+        }
     }
-}
+};
+Vue.createApp(SchatzApp).mount('#schatz-app');
